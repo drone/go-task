3b1004af2cf7ceabd1b82bbda3b06229421d5744
Vinicius Calasans <vinicius.calasans@harness.io>
2024-09-30 13:44:39 -0300

    Add expression resolver struct

---
+++ task/expression/evaler/evaler.go
@@ -0,0 +1,78 @@
+// Copyright 2024 Harness Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package evaler provides helper functions for evaluating
+// expressions in map structures.
+
+package evaler
+
+import (
+	"encoding/base64"
+	"regexp"
+	"strings"
+)
+
+// Eval evaluates expressions in the map structure.
+func Eval(data map[string]any, secrets map[string]string) {
+	var walk func(any) (bool, string)
+
+	// helper function to walk the map and inject
+	// secret variables in child keys where the
+	// value is a reference to a $secret.
+	walk = func(i any) (_ bool, _ string) {
+		switch v := i.(type) {
+		case string:
+			if !strings.Contains(v, "${{") {
+				return
+			}
+			v = resolveSecrets(v, secrets)
+			v = resolveGetAsBase64(v)
+			return true, v
+		case []any:
+			for i := 0; i < len(v); i++ {
+				if ok, updated := walk(v[i]); ok {
+					v[i] = updated
+				}
+			}
+		case map[string]any:
+			for key, value := range v {
+				if ok, updated := walk(value); ok {
+					v[key] = updated
+				}
+			}
+		}
+		return
+	}
+
+	// walk the map
+	walk(data)
+}
+
+func resolveSecrets(s string, secrets map[string]string) string {
+	// HACK(bradrydzewski) find/replace secrets
+	// until we have proper expression support.
+	for key, value := range secrets {
+		a := "${{secrets." + key + "}}"
+		b := value
+		s = strings.ReplaceAll(s, a, b)
+	}
+	return s
+}
+
+func resolveGetAsBase64(s string) string {
+	// regex to match the pattern ${{getAsBase64(...)}}
+	// the (?s) modifier will make the `.` also match line breaks ("\n")
+	re := regexp.MustCompile(`(?s)\$\{\{getAsBase64\((.*?)\)\}\}`)
+
+	// replace the matched strings with their base64-encoded values
+	return re.ReplaceAllStringFunc(s, func(match string) string {
+		// extract the string wrapped by ${{getAsBase64(...)}}
+		submatch := re.FindStringSubmatch(match)
+		if len(submatch) > 1 {
+			// Encode it to to base64
+			return base64.StdEncoding.EncodeToString([]byte(submatch[1]))
+		}
+		return match
+	})
+}
---
+++ task/expression/resolver.go
@@ -0,0 +1,35 @@
+package expression
+
+import (
+	"encoding/json"
+
+	"github.com/drone/go-task/task/expression/evaler"
+)
+
+type Resolver struct {
+	secrets map[string]string
+}
+
+func New(secrets map[string]string) *Resolver {
+	return &Resolver{secrets: secrets}
+}
+
+func (r *Resolver) Resolve(data []byte) ([]byte, error) {
+	v := map[string]any{}
+
+	// unmarshal the task data into a map
+	err := json.Unmarshal(data, &v)
+	if err != nil {
+		return nil, err
+	}
+
+	// evaluate the expressions
+	evaler.Eval(v, r.secrets)
+
+	// encode the map back to []byte
+	resolved, err := json.Marshal(v)
+	if err != nil {
+		return nil, err
+	}
+	return resolved, nil
+}
